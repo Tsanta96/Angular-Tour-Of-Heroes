5eed3c1ab54f84d071f379765d3b6d363a20c936
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-05-17 13:35:55 -0400

    Create and Display Mock heroes & create Master/Detail View

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 //@Component is a decorator function that specifies the Angular metadata for the component
 @Component({
@@ -7,15 +8,23 @@ import { Hero } from '../hero';
   // this component within a parent components template
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
+
+  //Styles and stylesheets identified in @Component metadata are scoped to that 
+  // specific component. The heroes.component.css styles apply only to the HeroesComponent 
+  // and don't affect the outer HTML or the HTML in any other component.
+  // styles: [], // Can define styles inline with this
   styleUrls: ['./heroes.component.css']
 })
 
 export class HeroesComponent implements OnInit {
   //Adding a hero property of type Hero to the HeroesComponent for a hero named "Windstorm."
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  //exposing HEROES array for binding
+  heroes = HEROES;
 
   constructor() { 
     
